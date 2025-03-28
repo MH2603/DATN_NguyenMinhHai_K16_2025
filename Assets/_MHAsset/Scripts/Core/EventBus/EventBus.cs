a3b1cb7e7504f1e79e90b522f5645350bda7e7de
MinhHai <minh.hai@playsync.net>
2025-03-28 18:38:57 +0700

    add ServiceLocator and EventBus

--- Assets/_MHAsset/Scripts/Core/EventBus/EventBus.cs
+++ Assets/_MHAsset/Scripts/Core/EventBus/EventBus.cs
@@ -1,54 +1,134 @@
+using UnityEngine.Events;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-/// <summary>
-/// Interface representing an event.
-/// </summary>
-public interface IEvent { }
-
-/// <summary>
-/// Static class for managing event bindings and raising events of type T.
-/// </summary>
-/// <typeparam name="T">The type of event, which must implement IEvent.</typeparam>
-public static class EventBus<T> where T : IEvent 
+namespace  MH.Core
 {
-    // A set of event bindings for the event type T.
-    static readonly HashSet<IEventBinding<T>> bindings = new HashSet<IEventBinding<T>>();
-    
-    /// <summary>
-    /// Registers an event binding.
-    /// </summary>
-    /// <param name="binding">The event binding to register.</param>
-    public static void Register(EventBinding<T> binding) => bindings.Add(binding);
 
-    /// <summary>
-    /// Deregisters an event binding.
-    /// </summary>
-    /// <param name="binding">The event binding to deregister.</param>
-    public static void Deregister(EventBinding<T> binding) => bindings.Remove(binding);
 
-    /// <summary>
-    /// Raises an event, invoking all registered bindings.
-    /// </summary>
-    /// <param name="event">The event to raise.</param>
-    public static void Raise(T @event) {
-        // Create a snapshot of the current bindings to avoid modification during iteration.
-        var snapshot = new HashSet<IEventBinding<T>>(bindings);
-
-        // Invoke each binding's event handlers.
-        foreach (var binding in snapshot) {
-            if (bindings.Contains(binding)) {
-                binding.OnEvent.Invoke(@event);
-                binding.OnEventNoArgs.Invoke();
-            }
-        }
+    public interface IEventContext
+    {
+        // This can be left empty as a marker interface
+        // Or you can add properties/methods if specific context data is needed
     }
 
     /// <summary>
-    /// Clears all event bindings.
+    /// Interface for an event bus that allows registering, unregistering, and sending events.
     /// </summary>
-    static void Clear() {
-        Debug.Log($"Clearing {typeof(T).Name} bindings");
-        bindings.Clear();
+    public interface IEventBus
+    {
+        /// <summary>
+        /// Registers a callback for a specific event type.
+        /// </summary>
+        /// <typeparam name="T">The event context type.</typeparam>
+        /// <param name="callback">The callback to register.</param>
+        void Register<T>(UnityAction<T> callback) where T : IEventContext;
+
+        /// <summary>
+        /// Unregisters a callback for a specific event type.
+        /// </summary>
+        /// <typeparam name="T">The event context type.</typeparam>
+        /// <param name="callback">The callback to unregister.</param>
+        void Unregister<T>(UnityAction<T> callback) where T : IEventContext;
+
+        /// <summary>
+        /// Sends an event to all registered listeners.
+        /// </summary>
+        /// <typeparam name="T">The event context type.</typeparam>
+        /// <param name="context">The event context to send.</param>
+        void Send<T>(T context) where T : IEventContext;
+    }
+
+    public class EventBus : IEventBus
+    {
+        #region --------- Fields -------
+
+        // Dictionary to store event type to callback mappings
+        // int is hashcode of UnityAction
+        private readonly Dictionary<Type, Dictionary<int, UnityAction<IEventContext>>> _eventMap = new();
+
+        #endregion
+
+        #region --------- Public Methods -------
+
+        /// <summary>
+        /// Registers a callback for a specific event type
+        /// </summary>
+        /// <typeparam name="T">The event context type</typeparam>
+        /// <param name="callback">The callback to register</param>
+        public void Register<T>(UnityAction<T> callback) where T : IEventContext
+        {
+            if (callback == null)
+            {
+                Debug.LogError("EventBus: Registered callback is null");
+            }
+
+            Type eventType = typeof(T);
+
+            if (!_eventMap.TryGetValue(eventType, out var eventHandlers))
+            {
+                eventHandlers = new Dictionary<int, UnityAction<IEventContext>>();
+                _eventMap[eventType] = eventHandlers;
+            }
+
+            int callbackHash = callback.GetHashCode();
+            eventHandlers[callbackHash] = (context) => callback((T)context);
+        }
+
+        /// <summary>
+        /// Unregisters a callback for a specific event type
+        /// </summary>
+        /// <typeparam name="T">The event context type</typeparam>
+        /// <param name="callback">The callback to unregister</param>
+        public void Unregister<T>(UnityAction<T> callback) where T : IEventContext
+        {
+            if (callback == null)
+            {
+                Debug.LogError("EventBus: Unregistered callback is null");
+            }
+
+            Type eventType = typeof(T);
+
+            if (_eventMap.TryGetValue(eventType, out var eventHandlers))
+            {
+                int callbackHash = callback.GetHashCode();
+                eventHandlers.Remove(callbackHash);
+            }
+        }
+
+        /// <summary>
+        /// Sends an event to all registered listeners
+        /// </summary>
+        /// <typeparam name="T">The event context type</typeparam>
+        /// <param name="context">The event context to send</param>
+        public void Send<T>(T context) where T : IEventContext
+        {
+            if (context == null)
+            {
+                Debug.LogError("EvenBus: send a context is null");
+            }
+
+            Type eventType = typeof(T);
+
+            if (_eventMap.TryGetValue(eventType, out var eventHandlers))
+            {
+                foreach (var handler in eventHandlers.Values)
+                {
+                    handler(context);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Clears all registered events and listeners
+        /// </summary>
+        public void Clear()
+        {
+            _eventMap.Clear();
+        }
+
+        #endregion
     }
 }
